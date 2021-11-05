3f8cac012d0f947cdbb69ddbdfb74e80aa972c45
Ann456 <popovaa769@gmail.com>
2021-11-05 17:52:39 +0200

    изменен код

--- js/task-09.js
+++ js/task-09.js
@@ -1,19 +1,13 @@
-const divEl = document.querySelector(".widget");
 const buttonEl = document.querySelector(".change-color");
 const spanEl = document.querySelector(".color");
 
-divEl.addEventListener("click", buttonElClick);
+buttonEl.addEventListener("click", buttonElClick);
+
+function buttonElClick() {
+  spanEl.textContent = getRandomHexColor();
+  document.body.style.background = getRandomHexColor();
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-const color = getRandomHexColor();
-
-function buttonElClick(event) {
-  spanEl.textContent = getRandomHexColor();
-  if (!event.target.classList.contains("change-color")) {
-    return;
-  }
-  return (document.body.style.background = color);
 }
