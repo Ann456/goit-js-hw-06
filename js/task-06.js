984f258afd634384d6a9dc30534fc8e1b10654cf
Ann456 <popovaa769@gmail.com>
2021-11-13 17:20:11 +0200

    добавлен новый вариант кода

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,24 @@
+//Вариант 1
+// const inputEl = document.querySelector("#validation-input");
+
+// inputEl.addEventListener("blur", event => {
+
+//     if (event.target.value.length === Number(inputEl.dataset.length)) {
+//         inputEl.classList.add("valid");
+//         inputEl.classList.remove("invalid");
+//     } else {
+//         inputEl.classList.add("invalid");
+//         inputEl.classList.remove("valid");
+//     }
+// });
+
+// Вариант 2
 const inputEl = document.querySelector("#validation-input");
 
-inputEl.addEventListener("blur", event => {
+function onInputFunction (event) {
+   event.target.value.length === Number(inputEl.dataset.length)?        
+   inputEl.classList.add("valid") : inputEl.classList.add("invalid"); 
+    if (!inputEl.value) inputEl.removeAttribute("class");
+};
 
-    if (event.target.value.length === Number(inputEl.dataset.length)) {
-        inputEl.classList.add("valid");
-        inputEl.classList.remove("invalid");
-    } else {
-        inputEl.classList.add("invalid");
-        inputEl.classList.remove("valid");
-    }
-});
+inputEl.addEventListener("blur", onInputFunction);
